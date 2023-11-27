56fad380878959383f136a5cd17e0ad7de15c643
ipad6202 <78675126+ipad6202@users.noreply.github.com>
2023-11-27 14:05:15 -0500

    Added Pivot Arm File stuff

--- example_projects/simulation_1323/src/main/java/frc/robot/subsystems/PivotArm/PivotArmIO.java
+++ example_projects/simulation_1323/src/main/java/frc/robot/subsystems/PivotArm/PivotArmIO.java
@@ -1,23 +1,61 @@
+package frc.robot.subsystems.pivotArm;
+
+import org.littletonrobotics.junction.AutoLog;
+
 public interface PivotArmIO {
-    public enum State {
-        MANUAL;
+    @AutoLog
+    public static class PivotArmIOInputs {
+        public double angle = 0.0;
+        public double angleRadsPerSec = 0.0;
+        public double appliedVolts = 0.0;
+        public double[] currentAmps = new double[] {};
+        public double[] tempCelsius = new double[] {};
     }
 
-    public default void setPosition(double setpoint) {}
+    public double PIVOT_ARM_MAX_ANGLE = 150.0;
+    public double PIVOT_ARM_MIN_ANGLE = -30.0;
 
-    public default void manualControl(double newSpeed) {}
+    /** Updates the set of loggable inputs. */
+    public default void updateInputs(PivotArmIOInputs inputs) {
+    }
 
-    public default void updateIO() {}
+    /** Run open loop at the specified voltage. */
+    public default void setVoltage(double motorVolts) {
+    }
 
-    public default void displayShuffleboardIO() {}
+    /** Returns the current distance measurement. */
+    public default double getAngle() {
+        return 0.0;
+    }
 
-    public default void tuningInitIO() {}
+    public default void setPIDConstants(double p, double i, double d, double ff) {
+    }
 
-    public default void tuningPeriodicIO() {}
+    /** Go to Setpoint */
+    public default void goToSetpoint(double setpoint) {
+    }
 
-    public default State getState() { return State.MANUAL; }
+    public default void setBrake(boolean brake) {
+    }
 
-    public default double getArmAngle() { return 0; }
+    public default boolean atSetpoint() {
+        return false;
+    }
+
+    public default void setP(double p) {}
+    
+    public default void setI(double i) {}
+
+    public default void setD(double d) {}
+
+    public default void setFF(double ff) {}
+
+    public default double getP() { return 0.0; }
+
+    public default double getI() { return 0.0; }
+
+    public default double getD() { return 0.0; }
+
+    public default double getFF() { return 0.0; }
 
 }
-
